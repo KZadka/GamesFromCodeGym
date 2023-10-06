e363acd08e083d79965b7f5427d1fce0d88f900b
KZadka <kzadka89@gmail.com>
2023-10-07 00:00:59 +0200

    Start the implementation of move feature for Snake object. Step 8/20 done.

--- 8.Games/src/com/codegym/games/snake/SnakeGame.java
+++ 8.Games/src/com/codegym/games/snake/SnakeGame.java
@@ -7,14 +7,23 @@ public class SnakeGame extends Game {
     public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
     private Snake snake;
+    private int turnDelay;
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
         createGame();
     }
 
+    @Override
+    public void onTurn(int x) {
+        snake.move();
+        drawScene();
+    }
+
     private void createGame() {
         snake = new Snake(WIDTH / 2, HEIGHT / 2);
+        turnDelay = 300;
+        setTurnTimer(turnDelay);
         drawScene();
     }
 
